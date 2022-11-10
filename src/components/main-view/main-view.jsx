3729f43f03192aacfce179eeddaf411623b7649e
Thom187 <tom.hoppe@gmx.de>
2022-11-10 13:42:09 +0100

    Implement client-side-routing using React Router

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Row, Col, Button } from 'react-bootstrap/';
 
 import { LoginView } from '../login-view/login-view';
@@ -15,8 +16,23 @@ export class MainView extends React.Component {
     super();
     this.state = {
       movies: [],
-      selectedMovie: null
+      user: null
     };
+  }
+
+  getMovies(token) {
+    axios.get('https://my-flix1987.herokuapp.com/movies', {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then(response => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   componentDidMount() {
@@ -29,22 +45,22 @@ export class MainView extends React.Component {
     }
   }
 
-  /*When a movie is clicked,this function is invoked
-  and updates the state of the `selectedMovie` *property to that movie*/
-  setSelectedMovie(newSelectedMovie) {
-    this.setState({
-      selectedMovie: newSelectedMovie
-    });
-  }
+  // /*When a movie is clicked,this function is invoked
+  // and updates the state of the `selectedMovie` *property to that movie*/
+  // setSelectedMovie(newSelectedMovie) {
+  //   this.setState({
+  //     selectedMovie: newSelectedMovie
+  //   });
+  // }
 
-  onRegistration(register) {
-    this.setState({
-      register
-    });
-  }
+  // onRegistration(register) {
+  //   this.setState({
+  //     register
+  //   });
+  // }
 
   /* When a user successfully logs in, this function updates
-  the `user` property in state to that *particular user*/
+  the `user` property in state to that particular user*/
   onLoggedIn(authData) {
     console.log(authData);
     this.setState({
@@ -64,63 +80,43 @@ export class MainView extends React.Component {
     });
   }
 
-  getMovies(token) {
-    axios.get('https://my-flix1987.herokuapp.com/movies', {
-      headers: { Authorization: `Bearer ${token}` }
-    })
-      .then(response => {
-        // Assign the result to the state
-        this.setState({
-          movies: response.data
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-
   render() {
-    const { movies, selectedMovie, user, register } = this.state;
+    const { movies, user } = this.state;
 
-    if (!register) return (<RegistrationView onRegistration={(register) => this.onRegistration(register)} />);
+    // if (!register) return (<RegistrationView onRegistration={(register) => this.onRegistration(register)} />);
 
 
     /* If there is no user, the LoginView is rendered. 
     If there is a user logged in, the user details are
     *passed as a prop to the LoginView*/
-    if (!user) return <LoginView onLoggedIn={user =>
-      this.onLoggedIn(user)} />;
+    if (!user) return (
+      <LoginView onLoggedIn={user =>
+        this.onLoggedIn(user)} />)
 
     // Before the movies have been loaded
     if (movies.length === 0) return <div className="main-view" />;
 
     return (
-      <Row className="main-view justify-content-md-center">
-        {/*If the state of `selectedMovie` is not null,
-        that selected movie will be returned otherwise,
-        all *movies will be returned*/}
-        {selectedMovie
-          ? (
-            <Col md={8}>
-              <MovieView movie={selectedMovie}
-                onBackClick={newSelectedMovie => {
-                  this.setSelectedMovie(newSelectedMovie);
-                }} />
+      <Router>
+        <Row className="main-view justify-content-md-center">
+          <Route exact path="/" render={() => {
+            return movies.map(m => (
+              <Col md={3} key={m._id}>
+                <MovieCard movie={m} />
+              </Col>
+            ))
+          }} />
+          <Route path="/movies/:movieId" render={({ match }) => {
+            return <Col md={8}>
+              <MovieView movie={movies.find(m => m._id === match.params.movieId)} />
             </Col>
-          )
-          :
-          movies.map(movie => (
-            <Col sm={6} md={4} lg={3}>
-              <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => {
-                this.setSelectedMovie(newSelectedMovie);
-              }} />
-            </Col>
-          ))
-        }
-        <Button onClick={() => {
-          this.onLoggedOut()
-        }}>Logout</Button>
-      </Row>
+          }} />
+
+          <Button onClick={() => {
+            this.onLoggedOut()
+          }}>Logout</Button>
+        </Row>
+      </Router>
     );
   }
 }
