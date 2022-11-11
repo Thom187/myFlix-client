5fe86f3ebf3cdfea3ed2ba72eff3f58f09543236
Thom187 <tom.hoppe@gmx.de>
2022-11-11 10:37:13 +0100

    Define content for DirectorView and GenreView

---
+++ src/components/director-view/director-view.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { Button, Col, Container, Row, Col } from "react-bootstrap";
+
+export class DirectorView extends React.Component {
+  render() {
+    const { director, onBackClick } = this.props;
+
+    return (
+      <Container>
+
+        <Row>
+          <Col className="label">Director:</Col>
+          <Col className="value">{director.name}</Col>
+        </Row>
+        <Row className="mt-3">
+          <Col className="label">Bio:</Col>
+          <Col className="value">{director.bio}</Col>
+        </Row>
+        <Row className="mt-3">
+          <Col className="label">Birthday:</Col>
+          <Col className="value">{director.birthday}</Col>
+        </Row>
+
+        <Button className="mt-3" onClick={() => {
+          onBackClick(null);
+        }}>Back</Button>
+
+      </Container>
+    )
+  }
+}
+
+DirectorView.propTypes = {
+  director: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    bio: PropTypes.string.isRequired,
+    birthday: PropTypes.string.isRequired
+  }).isRequired,
+};
---
+++ src/components/genre-view/genre-view.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { Button, Col, Container, Row, Col } from "react-bootstrap";
+
+export class GenreView extends React.Component {
+  render() {
+    const { genre, onBackClick } = this.props;
+
+    return (
+      <Container>
+
+        <Row>
+          <Col className="label">Genre:</Col>
+          <Col className="value">{genre.name}</Col>
+        </Row>
+        <Row className="mt-3">
+          <Col className="label">Description:</Col>
+          <Col className="value">{genre.description}</Col>
+        </Row>
+
+        <Button className="mt-3" onClick={() => {
+          onBackClick(null);
+        }}>Back</Button>
+
+      </Container>
+    )
+  }
+}
+
+GenreView.propTypes = {
+  genre: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
+}
