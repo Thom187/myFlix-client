a8b52161cee01251af16a97bf555e6714ce013a6
Thom187 <tom.hoppe@gmx.de>
2022-11-14 10:10:04 +0100

    Implement Navigation bar (navbar)

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
+
+export function Menubar({ user }) {
+
+  const onLoggedOut = () => {
+    localStorage.clear();
+    window.open('/', '_self');
+  }
+
+  const isAuth = () => {
+    if (typeof window == 'undefined') {
+      return false;
+    }
+    if (localStorage.getItem('token')) {
+      return localStorage.getItem('token');
+    } else {
+      return false;
+    }
+  };
+
+  return (
+
+    <Navbar className='main-nav' sticky='top' bg='light' expand='lg' variant='light' >
+      <Container>
+        <Navbar.Brand className='navbar-logo' href='/' >
+          myFlixMovie
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='ml-auto'>
+            {isAuth() && (
+              <Nav.Link href={'/users/${user}'}>
+                {user}
+              </Nav.Link>
+            )}
+            {isAuth() && (
+              <Button variant='link' onClick={() => {
+                this.onLoggedOut()
+              }}>
+                Logout
+              </Button>
+            )}
+            {!isAuth() && (
+              <Nav.Link href='/'>
+                Login
+              </Nav.Link>
+            )}
+            {!isAuth() && (
+              <Nav.Link href='/register'>
+                Sign in
+              </Nav.Link>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
